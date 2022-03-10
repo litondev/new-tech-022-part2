ba85276cb303414a34213d7d40a64b5865b7d9e4
litoninot12345@gmail.com <litoninot12345@gmail.com>
2022-03-10 12:47:55 +0700

    compelete reset password

--- react17/src/views/ResetPassword/index.js
+++ react17/src/views/ResetPassword/index.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import { useNavigate,Navigate, useSearchParams} from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import "./index.css";
 
 const ResetPasswordSchema = Yup.object()
     .shape({      
-        password_confirm : Yup.string()
+        password : Yup.string()
             .min(8, 'Too Short!')
             .max(50, 'Too Long!')
             .required('Required'),
-        new_password: Yup.string()
+        password_confirmation: Yup.string()
             .min(8, 'Too Short!')
             .max(50, 'Too Long!')
             .required('Required'),
@@ -25,14 +26,14 @@ const ResetPassword = () => {
     const [searchParams] = useSearchParams();
 
     const [form] = useState({
-        password_confirm : '',
-        email : searchParams.get("email") || "user1@gmail.com", 
-        new_password : '',
-        token : searchParams.get("token") || ""
+        password_confirmation : '',
+        email : searchParams.get("email") || "admin@admin.com", 
+        password : '',
+        token : searchParams.get("token") || "12345"
     })
 
     const onSubmit = (values,{setSubmitting}) => {            
-        window.$axios.post("/auth/reset-password",values)
+        window.$axios.post("/reset-password",values)
         .then(() => {
             setSubmitting(false)
             window.$toastr("success","Berhasil Reset Password")
@@ -46,52 +47,60 @@ const ResetPassword = () => {
     }
 
     return (
-        <div>
-            <h1>ResetPassword</h1>
+        <div className="flex justify-center items-center h-screen">
+            <div className="container-small">
+                <h1 className="text-4xl">ResetPassword</h1>
 
-            <Formik
-                initialValues={form}
-                validationSchema={ResetPasswordSchema}
-                onSubmit={onSubmit}>
-                {({isSubmitting,resetForm}) => (                
-                    <Form>                                      
-                        <div>
-                            <Field 
-                                type="password" 
-                                name="new_password" 
-                                placeholder="Password . . ."/>
+                <Formik
+                    initialValues={form}
+                    validationSchema={ResetPasswordSchema}
+                    onSubmit={onSubmit}>
+                    {({isSubmitting,resetForm}) => (                
+                        <Form>                                      
+                            <div className="pt-5">
+                                <Field 
+                                    type="password" 
+                                    name="password" 
+                                    placeholder="Password . . ."
+                                    className="bg-white p-2 border-b-2 border-indigo-500 input-focus-gone w-full"/>
 
-                            <ErrorMessage  
-                                name="new_password" 
-                                component="div" />
-                        </div>
+                                <ErrorMessage  
+                                    name="password" 
+                                    component="div" 
+                                    className="text-red-600 text-sm text-right w-full pt-3"/>
+                            </div>
 
-                        <div>
-                            <Field 
-                                type="password" 
-                                name="password_confirm" 
-                                placeholder="Password  Konfirmasi . . ."/>
+                            <div className="pt-5">
+                                <Field 
+                                    type="password" 
+                                    name="password_confirmation" 
+                                    placeholder="Password  Konfirmasi . . ."
+                                    className="bg-white p-2 border-b-2 border-indigo-500 input-focus-gone w-full"/>
 
-                            <ErrorMessage  
-                                name="password_confirm" 
-                                component="div" />
-                        </div>
+                                <ErrorMessage  
+                                    name="password_confirmation" 
+                                    component="div" 
+                                    className="text-red-600 text-sm text-right w-full pt-3"/>
+                            </div>
 
-                        <div>
-                            <button 
-                                type="submit" 
-                                disabled={isSubmitting}>
-                                {isSubmitting ? '...' : 'Submit'}
-                            </button>
-                            <button type="reset"
-                                onClick={resetForm}>
-                                Reset
-                            </button>
-                        </div>                       
-                    </Form>
-                    )
-                }
-            </Formik>
+                            <div className="pt-5 flex justify-end">
+                                <button 
+                                    type="submit" 
+                                    disabled={isSubmitting}
+                                    className="pr-5 pl-5 pt-2 pb-2 bg-indigo-500 rounded-md text-white text-sm ml-2">
+                                    {isSubmitting ? '...' : 'Submit'}
+                                </button>
+                                <button type="reset"
+                                    onClick={resetForm}
+                                    className="pr-5 pl-5 pt-2 pb-2 bg-orange-500 rounded-md text-white text-sm ml-2">
+                                    Reset
+                                </button>
+                            </div>                       
+                        </Form>
+                        )
+                    }
+                </Formik>
+            </div>
         </div>
     );
 }
